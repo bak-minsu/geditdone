d912d2b412f0ad20d1c5d75ebcb6880b7ee641a4
ank97 <ankdave97@gmail.com>
2019-09-24 16:19:33 -0400

    Update gedcom_test_US06_US07.py
    added setup and asserts for US06 and US07

--- src/gedcom_test_US06_US07.py
+++ src/gedcom_test_US06_US07.py
@@ -5,5 +5,30 @@ from datetime import date
 class DivorceBeforeDeath(unittest.TestCase):
     """Makes sure divorces occur before death"""
 
+    def setUp(self):
+        # self.indivudals = db.individuals
+        # self.families = db.families
+
+    def runTest(self):
+        """Makes sure all divorces occur before death"""
+        # for family in families:
+        #     self.assertTrue(family.divorce < db.individual(family.partner1)[1].death)
+        #     self.assertTrue(family.divorce < db.individual(family.partner2)[2].death)
+
+    def tearDown(self):
+        # self.families = None
+        # self.individual = None
+
 class LessThan150YearsOld(unittest.TestCase):
     """Makes sure nobody reaches 150 years old"""
+
+    def setUp(self):
+        # self.indivudals = db.individuals
+
+    def runTest(self):
+        """Makes sure that the difference between birth and death is less than 150 """
+        # for individual in individuals:
+            # self.assertTrue((individual.death.years-individual.birth.years < 150), "Some guy x is less than 150 years old")
+
+    def tearDown(self):
+        # self.individual = None
