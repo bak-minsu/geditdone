d6ec837c063fbd4908383fd9435630437747d8ea
Min Su Park <mpark3@stevens.edu>
2019-09-26 20:30:05 -0400

    Final validator file that collects all implementations

---
+++ src/gedcom_validator.py
@@ -0,0 +1,8 @@
+class Validator:
+    def __init__(self, individuals, families):
+        """Initializes the class with families and individuals"""
+        self.individuals = individuals
+        self.families = families
+
+    def validate(self):
+        pass
