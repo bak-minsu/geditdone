1d78bc6d0c1aa605a73b2493c60ea461fbcebcfb
Gabby <emailgabby@gmail.com>
2019-09-27 23:11:56 -0400

    US03 fixes and test cases

--- tests/stories/sprint1/US03.py
+++ tests/stories/sprint1/US03.py
@@ -6,8 +6,44 @@ from geditdone.stories import *
 
 class BirthBeforeDeath(unittest.TestCase):
     """Makes sure births are before death"""
+    def correct_order(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, date(1998, 2, 16), date(2015, 10, 6), None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertEqual(US03.birth_before_death(parser), True)
+
+    def incorrect_order(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, date(2015, 10, 6), date(1998, 2, 16), None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertEqual(US03.birth_before_death(parser), False)
+
+    def incorrect_birth_null(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, None, date(1998, 2, 16), None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertEqual(US03.birth_before_death(parser), False)
+
+    def correct_death_null(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, date(1998, 2, 16), None, None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertEqual(US03.birth_before_death(parser), True)
+
+    def correct_nulls(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, None, None, None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertEqual(US03.birth_before_death(parser), True)
+
     def runTest(self):
         self.correct_order()
         self.incorrect_order()
-        self.correct_nulls()
-        self.incorrect_nulls()
+        self.incorrect_birth_null()
+        self.correct_death_null()
+        self.correct_nulls
