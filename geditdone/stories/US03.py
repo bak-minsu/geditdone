1a91c65ee91b837d548aa796656e1a405d00487c
Gabby <emailgabby@gmail.com>
2019-09-27 23:37:50 -0400

    Reverted changes and updated US03

--- geditdone/stories/US03.py
+++ geditdone/stories/US03.py
@@ -2,7 +2,8 @@ def birth_before_death(parser):
     """Makes sure births are before death"""
     individuals = parser.individuals
     for individual in individuals.values():
-        if individual.death is not None and \
+        if individual.birth is not None and \
+            individual.death is not None and \
             individual.birth >= individual.death:
             return False
         elif individual.birth is None and individual.death is not None:
