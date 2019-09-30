2d0a09c6c438df12343def66482ad62a8747ccdf
ank97 <ankdave97@gmail.com>
2019-09-29 22:37:14 -0400

    Logic Updates/Fixes

--- geditdone/stories/US07.py
+++ geditdone/stories/US07.py
@@ -1,15 +1,24 @@
+from datetime import datetime
+
 def less_than_150_years_old(parser):
     """Makes sure nobody reaches 150 years old"""
     individuals = parser.individuals
     for individual in individuals.values():
-        # birth true, death true, less than 150
-        if individual.birth is not None and \
-            individual.death is not None and \
-            individual.death.yearInt-individual.birth.yearInt >= 150:
-            return False
-        # birth true, death false, less than 150
-        else if individual.birth is not None and \
-            individual.death is None and \
-            individual.death.yearInt-individual.birth.yearInt >= 150:
-            return False
+        # print("---------------------------------------------------------")
+        # if birth is true
+        if individual.birth is not None:
+            individual_birth_year=(datetime.strptime(str(individual.birth), '%Y-%m-%d')).year
+            # print("individual_birth_year: ",individual_birth_year)
+            # if death is true (dead)
+            if individual.death is not None:
+                individual_death_year=(datetime.strptime(str(individual.death), '%Y-%m-%d')).year
+                # print("individual_death_year: ",individual_death_year)
+                if (individual_death_year-individual_birth_year >= 150):
+                    return False
+            # if death is not true (still alive)
+            else:
+                current_date_year=datetime.today().year
+                # print("current_date_year: ",current_date_year)
+                if (current_date_year-individual_birth_year >= 150):
+                    return False
     return True
