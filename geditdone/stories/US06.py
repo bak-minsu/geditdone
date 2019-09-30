c1d1c43ea183b3139fe2de1bfd00f15db06bc895
ank97 <ankdave97@gmail.com>
2019-09-29 22:41:11 -0400

    Updated logic

--- geditdone/stories/US06.py
+++ geditdone/stories/US06.py
@@ -1,11 +1,15 @@
 def divorce_before_death(parser):
-    """Makes sure divorces occur before death"""
+    """Makes sure divorces come before death"""
     individuals = parser.individuals
     families = parser.families
     for individual in individuals.values():
-        for family in families.values():
-            if family.divorced is not None and \
+        if individual.fams is not None:
+            fam = families.get(individual.fams)
+            if fam.divorced is not None and \
                 individual.death is not None and \
-                family.divorced >= individual.death:
-                return False
+                fam.divorced >= individual.death:
+                    return False
+            elif fam.divorced is None and \
+                individual.death is not None:
+                    return False
     return True
