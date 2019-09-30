e3c61e4fb6365e4cf97d656c8a762a3695cb5772
ank97 <ankdave97@gmail.com>
2019-09-30 00:42:08 -0400

    base code added

--- geditdone/stories/US01.py
+++ geditdone/stories/US01.py
@@ -1,5 +1,24 @@
+from datetime import datetime
+
 def dates_before_current_date(parser):
     """Makes sure all dates are before current date"""
     individuals = parser.individuals
     families = parser.families
+
+    current_date = datetime.today()
+
+    # check all individual dates: birth, death
+    for individual in individuals.values():
+        if individual.birth is not None and individual.death is not None:
+            if individual.birth >= current_date or \
+                individual.death >= current_date:
+                return False
+
+    # check all family dates: marriage, divorce
+    for family in families.values():
+        if family.married is not None and family.divorced is not None:
+            if family.married >= current_date or \
+                family.divorced >= current_date:
+                return False
+
     return True
