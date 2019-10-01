01cf1ab7aad66954524a59cc5bbebb8c8a7890f8
bak-minsu <minsupark@live.com>
2019-10-01 14:33:06 -0400

    Added tests

--- tests/stories/sprint1/US07.py
+++ tests/stories/sprint1/US07.py
@@ -6,5 +6,12 @@ from geditdone.stories import US07
 
 class LessThan150YearsOld(unittest.TestCase):
     """Makes sure nobody reaches 150 years old"""
+    def incorrect_age(self):
+        individuals = {
+            "TEST1": Individual("TEST1", None, None, date(1800, 2, 16), date(2015, 10, 6), None, None)
+        }
+        parser = TestParser(individuals, {})
+        self.assertGreater(len(US07.less_than_150_years_old(parser)), 0)
+
     def runTest(self):
-        pass
+        self.incorrect_age()
