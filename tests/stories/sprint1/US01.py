d8df4b8d7245a6b1f8e4f7686fe2634cd9b532f7
Min Su Park <mpark3@stevens.edu>
2019-10-01 02:07:25 -0400

    Added new tests

--- tests/stories/sprint1/US01.py
+++ tests/stories/sprint1/US01.py
@@ -6,7 +6,7 @@ from geditdone.stories import US01
 
 class DatesBeforeCurrentDate(unittest.TestCase):
 
-    def individual_dates_after_today(self):
+    def incorrect_individuals(self):
         individuals = {
             "TEST1": Individual("TEST1", name=None, sex=None, birth=None, death=date(2030, 11, 6), famc=None, fams="TEST1"),
             "TEST2": Individual("TEST2", name=None, sex=None, birth=date(2035, 11, 6), death=None, famc=None, fams="TEST1")
@@ -14,14 +14,14 @@ class DatesBeforeCurrentDate(unittest.TestCase):
         parser = TestParser(individuals, {})
         self.assertGreater(len(US01.dates_before_current_date(parser)), 0)
 
-    def family_dates_after_today(self):
+    def incorrect_families(self):
         families = {
             "TEST1": Family("TEST1", [], None, None, date(2044, 10, 5), None),
             "TEST2": Family("TEST2", [], None, None, None, date(2054, 10, 5))
         }
         parser = TestParser({}, families)
         self.assertGreater(len(US01.dates_before_current_date(parser)), 0)
-        pass
 
     def runTest(self):
-        pass
+        self.incorrect_individuals()
+        self.incorrect_families()
--- tests/stories/sprint1/US02.py
+++ tests/stories/sprint1/US02.py
@@ -5,7 +5,18 @@ from geditdone.gedcom_objects import Family, Individual
 from geditdone.stories import US02
 
 class BirthBeforeMarriage(unittest.TestCase):
-    """Makes sure births are before marriage"""
-    def births_before_marriage(self):
-        """Checks both people in the marriage were born before the marriage"""
-        pass
+    """Makes sure births are after marriage"""
+    def birth_before_marriage(self):
+        individuals = {
+            "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "SON": Individual("SON", name=None, sex=None, birth=date(2010, 11, 6), death=None, famc="FAM", fams=None),
+        }
+        families = {
+            "FAM": Family("FAM", ["SON"], None, None, date(2054, 10, 5), None)
+        }
+        parser = TestParser(individuals, families)
+        self.assertGreater(len(US02.birth_before_marriage(parser)), 0)
+
+    def runTest(self):
+        self.birth_before_marriage()
