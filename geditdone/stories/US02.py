0d5daa04ed17a8bc8de45a3f205d4122ccddaad9
maxlep <mlepkows@stevens.edu>
2019-10-01 19:04:03 -0400

    Update logic in US02. Add more specific edge cases to US01 and US02 unit tests.

--- geditdone/stories/US02.py
+++ geditdone/stories/US02.py
@@ -14,9 +14,5 @@ def birth_before_marriage(parser):
                 individual.birth <= fam.married:
                     errorMessage = f'Birth date {individual.birth} occurs before marriage date {fam.married}'
                     errors.append(GedcomError(GedcomError.ErrorType.error, 'US02', fam, errorMessage))
-            # elif individual.birth is None and family.married is not None:
-            #     # TODO talk about this logic, not sure it makes sense
-            #     errorMessage = f'Does not have a birth date before marriage date {family.married}'
-            #     errors.append(GedcomError(GedcomError.ErrorType.error, 'US02', family, errorMessage))
 
     return errors
--- tests/stories/sprint1/US02.py
+++ tests/stories/sprint1/US02.py
@@ -5,18 +5,69 @@ from geditdone.gedcom_objects import Family, Individual
 from geditdone.stories import US02
 
 class BirthBeforeMarriage(unittest.TestCase):
-    """Makes sure births are after marriage"""
-    def birth_before_marriage(self):
+
+    def no_children(self):
+        individuals = {
+            "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM")
+        }
+        families = {
+            "FAM": Family("FAM", [], None, None, date(2054, 10, 5), None)
+        }
+        parser = TestParser(individuals, families)
+        self.assertEqual(len(US02.birth_before_marriage(parser)), 0)
+
+    def child_no_birth_date(self):
+        individuals = {
+            "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "SON": Individual("SON", name=None, sex=None, birth=None, death=None, famc="FAM", fams=None),
+        }
+        families = {
+            "FAM": Family("FAM", ["SON"], None, None, date(2054, 10, 5), None)
+        }
+        parser = TestParser(individuals, families)
+        self.assertEqual(len(US02.birth_before_marriage(parser)), 0)
+
+    def child_birth_after_marriage(self):
+        individuals = {
+            "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "SON": Individual("SON", name=None, sex=None, birth=date(2011, 6, 6), death=None, famc="FAM", fams=None),
+        }
+        families = {
+            "FAM": Family("FAM", ["SON"], None, None, date(2010, 9, 5), None)
+        }
+        parser = TestParser(individuals, families)
+        self.assertEqual(len(US02.birth_before_marriage(parser)), 0)
+
+    def child_birth_at_marriage(self):
         individuals = {
             "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
             "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
             "SON": Individual("SON", name=None, sex=None, birth=date(2010, 11, 6), death=None, famc="FAM", fams=None),
         }
         families = {
-            "FAM": Family("FAM", ["SON"], None, None, date(2054, 10, 5), None)
+            "FAM": Family("FAM", ["SON"], None, None, date(2010, 11, 6), None)
         }
         parser = TestParser(individuals, families)
-        self.assertGreater(len(US02.birth_before_marriage(parser)), 0)
+        self.assertEqual(len(US02.birth_before_marriage(parser)), 1)
+
+    def child_birth_before_marriage(self):
+        individuals = {
+            "DAD": Individual("DAD", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "MOM": Individual("MOM", name=None, sex=None, birth=None, death=None, famc=None, fams="FAM"),
+            "SON": Individual("SON", name=None, sex=None, birth=date(2010, 11, 6), death=None, famc="FAM", fams=None),
+        }
+        families = {
+            "FAM": Family("FAM", ["SON"], None, None, date(2012, 10, 5), None)
+        }
+        parser = TestParser(individuals, families)
+        self.assertEqual(len(US02.birth_before_marriage(parser)), 1)
 
     def runTest(self):
-        self.birth_before_marriage()
+        self.no_children()
+        self.child_no_birth_date()
+        self.child_birth_after_marriage()
+        self.child_birth_at_marriage()
+        self.child_birth_before_marriage()
