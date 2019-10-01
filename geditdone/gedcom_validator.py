be8a3a54d68e234276bc397ff6a0ff17ed501e08
Min Su Park <mpark3@stevens.edu>
2019-10-01 01:55:10 -0400

    Improved valid function output

--- geditdone/gedcom_validator.py
+++ geditdone/gedcom_validator.py
@@ -41,4 +41,4 @@ class Validator:
     # for debugging
     def valid(self, function):
         """What to do when the validator returns valid"""
-        print("Function {} successfully passed!".format(function.__name__))
+        print("Validator function {} successfully passed!".format(function.__name__))
