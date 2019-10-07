750d797dd1192ae510f0ee532b71eda92dd2e2ca
Min Su Park <minsupark@live.com>
2019-10-06 22:22:49 -0400

    Updated database

--- geditdone/gedcom_db.py
+++ geditdone/gedcom_db.py
@@ -2,61 +2,72 @@ import pandas as pd
 
 class GedcomDatabase:
 
-    def __init__(self, individuals, families):
-        self.individuals = self.gen_individual_dict(individuals)
-        self.families = self.gen_families_dict(families)
+    def __init__(self, parser):
+        """Individual pandas dataframes for individual and family"""
+        # To understand how to use these, look up "Pandas Dataframes"
+        self.individuals = self.gen_individual_dict(parser.individuals)
+        self.families = self.gen_families_dict(parser.families)
         
     def gen_individual_dict(self, individuals):
 
+        all_ids = []
         all_names = []
         all_sexes = []
         all_births = []
         all_deaths = []
         all_famc = []
         all_fams = []
+        all_references = []
 
-        for individual in individuals.values():
+        for ID, individual in individuals.items():
+            all_ids.append(ID)
             all_names.append(individual.name)
             all_sexes.append(individual.sex)
             all_births.append(individual.birth)
             all_deaths.append(individual.death)
             all_famc.append(individual.famc)
             all_fams.append(individual.fams)
+            all_references.append(individual)
 
         data = {
-            "ID": individuals.keys(), 
-            "Name": all_names,
-            "Sex": all_sexes, 
-            "Birth": all_births,
-            "Death": all_deaths, 
-            "Famc": all_famc, 
-            "Fams": all_fams
+            "ID": all_ids, 
+            "name": all_names,
+            "sex": all_sexes, 
+            "birth": all_births,
+            "death": all_deaths, 
+            "famc": all_famc, 
+            "fams": all_fams,
+            "reference": all_references,    # Reference to the Individual class
         }
         
         return pd.DataFrame(data)
 
     def gen_families_dict(self, families):
 
+        all_ids = []
         all_children = []
         all_husbands = []
         all_wives = []
         all_marriages = []
         all_divorces = []
+        all_references = []
 
-        for family in families.values():
+        for ID, family in families.items():
+            all_ids.append(ID)
             all_children.append(family.child_ids)
             all_husbands.append(family.husband_id)
             all_wives.append(family.wife_id)
             all_marriages.append(family.married)
             all_divorces.append(family.divorced)
+            all_references.append(family)
 
         data = {
-            "ID": families.keys(),
-
-            "Husband": all_husbands,
-            "Wives": all_wives,
-            "Married": all_marriages,
-            "Divorce": all_divorces
+            "ID": all_ids,
+            "husband": all_husbands,
+            "wife": all_wives,
+            "married": all_marriages,
+            "divorced": all_divorces,
+            "reference": all_references,    # Reference to the Family class
         }
 
         return pd.DataFrame(data)
