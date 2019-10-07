56688eba0f7ff85cd8991259aed29a3aabef034a
Min Su Park <minsupark@live.com>
2019-10-06 22:24:42 -0400

    Updated validator to choose db the right function

--- geditdone/gedcom_validator.py
+++ geditdone/gedcom_validator.py
@@ -1,19 +1,28 @@
 import sys
 import inspect
 from geditdone.gedcom_objects import GedcomError
-# Try not to import unnecessary packages
 from geditdone.stories import *
+from geditdone.gedcom_db import GedcomDatabase
 
 class Validator:
     def __init__(self, parser):
         """Initializes the class with families and individuals"""
         self.parser = parser
+        self.db = GedcomDatabase(parser)
+
+    def get_argument_count(self, function):
+        return len(inspect.getargspec(function)[0])
 
     # TODO: Return an error when invalid
     def validate(self):
         stories = self.get_all_stories()
         for story in stories:
-            errors = story(self.parser)
+            errors = None
+            argument_count = self.get_argument_count(story)
+            if argument_count == 1:
+                errors = story(self.parser)
+            elif argument_count == 2:
+                errors = story(self.parser, self.db)
             if len(errors) > 0:
                 self.invalid(story, errors)
             # else:
@@ -27,7 +36,9 @@ class Validator:
 
         for module in modules:
             if "geditdone.stories." in module:
+                # Gets a list of functions from the module, [-4:] is to get the last 4 characters, "US01" for example.
                 function_list = eval("inspect.getmembers({}, inspect.isfunction)".format(module[-4:]))
+                # List of tuples representing functions, where the second item is the reference to the function
                 functions.append(function_list[0][1])
 
         # print(functions)
