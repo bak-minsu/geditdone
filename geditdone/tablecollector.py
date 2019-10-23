980173a1e36b716f55470d7d159f34026490b381
Min Su Park <minsupark@live.com>
2019-10-23 13:49:21 -0400

    Added comments to TableCollector for clarity

--- geditdone/tablecollector.py
+++ geditdone/tablecollector.py
@@ -5,16 +5,17 @@ class TableCollector:
 
     @classmethod
     def add_dataframe(cls, dataframe, table_name):
+        """Converts and Pandas Dataframe to the tables dictionary"""
         pt = PrettyTable()
         columns = list(dataframe.columns) 
-        columns.remove("reference") # Reference to individual/family class is unnecessary
-        pt.field_names = columns
+        columns.remove("reference")             # Reference to individual/family class is unnecessary
+        pt.field_names = columns                # Set PrettyTable columns headers
         for _, row in dataframe.iterrows():
-            row_items = []
+            row_items = []                      # Convert pandas dataframe row to python list
             for column in columns:
                 row_items.append(row[column])
-            pt.add_row(row_items)
-        TableCollector.tables[table_name] = pt
+            pt.add_row(row_items)               # Add the list-row to PrettyTable
+        TableCollector.tables[table_name] = pt  # Add the PrettyTable to TableCollector list
 
     @classmethod
     def print_all(cls):
