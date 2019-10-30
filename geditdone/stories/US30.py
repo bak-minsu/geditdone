2d9f5379a8ded136eddeb08d82b7f66dc3c39543
ank97 <ankdave97@gmail.com>
2019-10-29 20:02:36 -0400

    buggy 30 and 31

--- geditdone/stories/US30.py
+++ geditdone/stories/US30.py
@@ -3,16 +3,16 @@ from geditdone.error_objects import GedcomError, ErrorType
 
 def list_living_married(parser, db):
     tables = []
-    individuals = db.individuals
-    #individuals = individuals.loc[individuals["death"].isnull()]
-
+    #individuals = db.individuals
     individuals = parser.individuals
     families = parser.families
-    errors = []
+
+    # store captured living married individuals
     married_individuals = []
 
     for fam in families.values():
         if fam.married:
+            # find wives and husbands and add to list of names
             for individual in individuals.values():
                 if (individual.id == individuals.get(fam.wife_id) or individual.id == individuals.get(fam.husband_id)):
                     married_individuals.append(individual.name)
--- geditdone/stories/US31.py
+++ geditdone/stories/US31.py
@@ -1,19 +1,24 @@
 from geditdone.tablehelpers import TableHelpers
 from datetime import datetime
+#from dateutil.relativedelta import relativedelta
 
 # over 30 and never married
 def list_living_single(parser, db):
     tables = []
-    individuals = db.individuals
+    #individuals = db.individuals
+    individuals = parser.individuals
 
     single_individuals = []
+
 
     for individual in individuals:
         individual_birth_year=(datetime.strptime(str(individual.birth), '%Y-%m-%d')).year
         individual_death_year=(datetime.strptime(str(individual.death), '%Y-%m-%d')).year
         age = individual_death_year-individual_birth_year
         if (age >= 30):
-            single_individuals.append(individual.name)
+            # if no spouse found, add name to list of names
+            if individual.fams is None:
+                single_individuals.append(individual.name)
 
     pt = TableHelpers.dataframe2table(single_individuals, "Living Single Individuals")
     tables.append(pt)
