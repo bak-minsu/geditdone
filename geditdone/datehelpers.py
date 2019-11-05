5e3780f3423eab8e63aa4419a6f1b8031cd1bfdd
bak-minsu <minsupark@live.com>
2019-11-04 22:04:59 -0500

    Added change_year to datehelpers

--- geditdone/datehelpers.py
+++ geditdone/datehelpers.py
@@ -1,4 +1,5 @@
 from dateutil.relativedelta import relativedelta
+from datetime import date
 
 class DateHelpers:
     @classmethod
@@ -16,6 +17,14 @@ class DateHelpers:
             limit is a number
             units is a string in ('days','months','years')'''
         return DateHelpers.date_diff(date1, date2, units) <= limit
+    
+    @classmethod
+    def change_year(cls, prev_date, year):
+        """returns a new datetime.date instance with an updated year
+            date is a datetime.date instance
+            year is a number
+        """
+        return date(year, prev_date.month, prev_date.day)
 
     @classmethod
     def calculate_age(cls, date, reference):
