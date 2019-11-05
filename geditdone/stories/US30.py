ce044f9b1fd9f4b8b712de7522ee9295aea4ce9f
ank97 <ankdave97@gmail.com>
2019-11-05 15:58:00 -0500

    change title, listify tables US30/31

--- geditdone/stories/US30.py
+++ geditdone/stories/US30.py
@@ -5,11 +5,16 @@ from prettytable import PrettyTable
 
 def list_living_married(parser, db):
 
+    tables = []
+
     my_table = PrettyTable()
-    my_table.title = "List Living Married"
+    my_table.title = "US30: List Living Married"
     my_table.field_names = ["id", "name", "sex", "birth", "death", "famc","fams"]
 
     for person in parser.individuals.values():
         if ((person.fams is not None) and (person.death is None)):
             my_table.add_row([person.id, person.name, person.sex, person.birth, person.death, person.famc, person.fams])
-    return my_table
+
+    tables.append(my_table)
+
+    return tables
