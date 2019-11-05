919c53ee38da4b2e58754e04e37e5593e39d1cfc
bak-minsu <minsupark@live.com>
2019-11-04 22:05:14 -0500

    Added Story number label

--- geditdone/stories/US29.py
+++ geditdone/stories/US29.py
@@ -6,6 +6,6 @@ def list_deceased(parser, db):
     today = date.today()
     individuals = db.individuals
     individuals = individuals.loc[(~individuals["death"].isnull())] # ~ is the loc equivalent of "not"
-    pt = TableHelpers.dataframe2table(individuals, "Deceased Individuals")
+    pt = TableHelpers.dataframe2table(individuals, "US29 - Deceased Individuals")
     tables.append(pt)
     return tables
