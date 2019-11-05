a9662f72f9d54d92d9895c8a5a44cba620e8edd7
ank97 <ankdave97@gmail.com>
2019-11-05 15:44:19 -0500

    update US34 to support architecture

--- tests/stories/sprint4/US34.py
+++ tests/stories/sprint4/US34.py
@@ -23,7 +23,7 @@ class ListLargeAgeDifferences(unittest.TestCase):
         db = TestDatabase(individuals, families)
         pt_list = US34.list_large_age_differences(parser, db)
         #print(pt_list)
-        self.assertEqual(TableHelpers.get_row_count(pt_list), 2)
+        self.assertEqual(TableHelpers.get_row_count(pt_list[0]), 2)
 
     def runTest(self):
         self.large_age_differences()
